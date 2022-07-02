7b58fb3f215181bed4d91e9c942ecff9e1747bc5
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2022-07-02 14:58:53 +0900

    Create 1158.py

---
+++ 1158.py
@@ -0,0 +1,13 @@
+import sys
+num = list(map(int,sys.stdin.readline().split()))
+people = [i for i in range(1,num[0]+1)]
+result = []
+index = num[1] -1
+
+while len(people):
+    if index >= len(people):
+        index = index % len(people)
+    else:
+        result.append(str(people.pop(index)))
+        index += num[1] -1
+print("<", ", ".join(result), ">",sep='')
