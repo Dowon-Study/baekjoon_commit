c73a783da0d1bf2d3def0b7cc74fafea5ba34e6d
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2022-07-09 14:50:48 +0900

    Create 10820.py

---
+++ 10820.py
@@ -0,0 +1,21 @@
+import sys
+
+while 1:
+
+    string = list(sys.stdin.readline().rstrip('\n'))
+    
+    if not string:
+        break
+
+    result = [0] * 4
+    for i in string:
+        if 65 <= ord(i) <= 91:
+            result[1] += 1
+        elif 97 <= ord(i) <= 123:
+            result[0] += 1
+        elif 48 <= ord(i) <= 58:
+            result[2] += 1
+        else:
+            result[3] += 1
+
+    print(*result)
