e20189ce8b68b939f4eed76571e2df33157c6e9b
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2022-07-11 21:56:52 +0900

    Create 10809.py

---
+++ 10809.py
@@ -0,0 +1,11 @@
+import sys
+
+string = list(sys.stdin.readline().rstrip())
+
+answer = [-1] * 26
+
+for i in range(len(string)):
+    if answer[ord(string[i]) - 97] == -1:
+        answer[ord(string[i]) - 97] = i
+        
+print(*answer)
