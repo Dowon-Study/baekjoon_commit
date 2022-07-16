ac0adc33285c70d1c76e33821953acbc565c794a
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2022-07-16 19:06:33 +0900

    Create 1463.py

---
+++ 1463.py
@@ -0,0 +1,9 @@
+n = int(input())
+answer = [0] * (n+1)
+for i in range(2, n+1):
+    answer[i] = answer[i-1] + 1
+    if i % 2 == 0:
+        answer[i] = min(answer[i], answer[i//2]+1)
+    if i % 3 == 0:
+        answer[i] = min(answer[i], answer[i//3] + 1)
+print(answer[n])
