25c6f447302d46548d778cb54e919ad6740e453d
Dowon Yun <90703740+Dowon-Study@users.noreply.github.com>
2022-08-27 13:20:54 +0900

    Update 10809.py

--- 10809.py
+++ 10809.py
@@ -6,6 +6,6 @@ answer = [-1] * 26
 
 for i in range(len(string)):
     if answer[ord(string[i]) - 97] == -1:
-        answer[ord(string[i]) - 97] = i
+        answer[ord(string[i]) - 97] = i 
         
 print(*answer) 
